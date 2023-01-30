5069f1bb44bcbbdfc731333c24a39be1b2e1f370
Megan <megan_mccarty@hotmail.com>
2023-01-29 19:11:48 -0500

    Remove TailwindCSS, blank README

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 module.exports = function(eleventyConfig) {
     // Reload page when changes made to .scss, .css, and .js files
     eleventyConfig.addWatchTarget('./src/scss/');
-    eleventyConfig.addWatchTarget('./src/css/');
     eleventyConfig.addWatchTarget('./src/js/');
     eleventyConfig.setBrowserSyncConfig({
         files: './dist/css/**/*.css',
